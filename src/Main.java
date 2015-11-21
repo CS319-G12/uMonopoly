f0c510b0b37c97b6e9004654fcd03bd22c3adb98
anikristo <anikristo@gmail.com>
2015-11-21 21:28:28 +0200

    rebasement

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,6 @@ package src;
  * @author
  */
 public class Main{
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
