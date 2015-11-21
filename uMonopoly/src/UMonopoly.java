2f22dfd9f4e2f83ac9c3ce17551981c662cca999
anikristo <anikristo@gmail.com>
2015-11-21 12:21:32 +0200

    Initiating project

---
+++ uMonopoly/src/UMonopoly.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author anikristo
+ */
+public class UMonopoly {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    
+}
