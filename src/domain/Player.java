6272ca6c22c4d7cf382cd07ebda505910c431b0e
anikristo <anikristo@gmail.com>
2015-11-23 18:24:14 +0200

    Skeleton

--- src/domain/Player.java
+++ src/domain/Player.java
@@ -6,9 +6,20 @@ package src.domain;
 public class Player {
 
     // ATTRIBUTES
+    private String name;
 
     // CONSTRUCTOR
+    public Player(String name) {
+        this.name = name;
+    }
 
     // METHODS
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
 }
