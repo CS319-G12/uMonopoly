fbf5aa1f9a9b7b862ce23a8d6adf318393a4a8c3
buseburcuakcadag <buse.b.akcadag@gmail.com>
2015-11-24 16:18:44 +0200

    implementation

--- src/domain/squares/CommunityChestCardSquare.java
+++ src/domain/squares/CommunityChestCardSquare.java
@@ -1,14 +1,20 @@
 package domain.squares;
 
+import domain.cards.BonusCard;
+
 /**
  * Created by
  */
 public class CommunityChestCardSquare extends Square implements CardSquare {
+
 
     // ATTRIBUTES
 
     // CONSTRUCTOR
 
     // METHODS
-
+    @Override
+    public BonusCard pickCard() {
+        return null; // TODO
+    }
 }
